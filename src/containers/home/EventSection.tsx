9bdf73003059dcec60ca61ccd4c62480c096026e
ahargunyllib <billy.bpm03@gmail.com>
2024-09-27 09:43:19 +0700

    feat: Add home page components and assets

---
+++ src/containers/home/EventSection.tsx
@@ -0,0 +1,38 @@
+import SparkEvent from "@/assets/svgs/home/spark-event.svg";
+import Image from "next/image";
+
+export default function EventSection() {
+  return (
+    <main className="h-[1025px] w-full">
+      <section className="relative w-min top-[31px] left-[235px]">
+        <div className="w-[607px] h-[336px] absolute">
+          <div className="space-y-4 absolute bottom-0 z-20 w-full">
+            <p className="font-neueHaasDisplay font-[300] italic text-[56px] ">
+              Choose your
+            </p>
+            <h4 className="font-neueHaasDisplay text-8xl text-end">
+              Experience
+            </h4>
+          </div>
+          <div className="w-full">
+            <Image
+              src={SparkEvent}
+              alt="Spark Event"
+              className="absolute top-0 right-[78px] z-10"
+            />
+          </div>
+        </div>
+      </section>
+      <section className="relative top-[460px]">
+        <div className="flex flex-row gap-6 overflow-x-auto max-w-min ml-24">
+          {Array.from({ length: 9 }).map((_, index) => (
+            <div
+              key={index}
+              className="w-[486px] h-[502px] rounded-[32px] bg-[#1D1A1B] flex-shrink-0"
+            />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+}
---
+++ src/containers/home/HeroSection.tsx
@@ -0,0 +1,76 @@
+import ArrowRightBottom from "@/assets/svgs/home/arrow-right-bottom.svg";
+import HeroSectionBg from "@/assets/svgs/home/hero-section-bg.svg";
+import ImgScroll1 from "@/assets/svgs/home/img-scroll-1.svg";
+import ImgScroll2 from "@/assets/svgs/home/img-scroll-2.svg";
+import ImgScroll3 from "@/assets/svgs/home/img-scroll-3.svg";
+import ImgScroll4 from "@/assets/svgs/home/img-scroll-4.svg";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+export default function HeroSection() {
+  return (
+    <main className={cn("bg-[#17181A]", "h-[1025px] overflow-hidden")}>
+      <section className="relative w-full">
+        <div className="absolute top-80 left-24 w-[1000px] text-white z-20">
+          <div className="space-y-[100px]">
+            <div className="">
+              <h1 className={cn("font-instrumentSans font-[700] text-8xl")}>
+                Get Ready For
+              </h1>
+              <h3
+                className={cn(
+                  "font-neueHaasDisplay font-[300] italic text-7xl"
+                )}
+              >
+                Unforgettable
+              </h3>
+              <div className="h-5" />
+              <h2 className={cn("font-neueHaasDisplay text-9xl text-end")}>
+                Event Experience
+              </h2>
+            </div>
+            <div className="gap-[370px] flex flex-row">
+              <div className="size-[140px] rounded-full bg-[#FF4D09] flex justify-center items-center">
+                <Image src={ArrowRightBottom} alt="Arrow Right Bottom" />
+              </div>
+              <div className="mt-auto">
+                <p className="font-neueHaasDisplay text-end w-max text-[32px] opacity-70">
+                  Buying tickets through our <br /> platform is easy
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="image-scroll" className="w-[841px] h-[2012px] absolute top-[165px] right-[38px] z-10">
+          <Image
+            src={ImgScroll1}
+            alt="Image Scroll 1"
+            className="absolute top-0 left-0"
+          />
+          <Image
+            src={ImgScroll2}
+            alt="Image Scroll 2"
+            className="absolute top-[604px] right-0"
+          />
+          <Image
+            src={ImgScroll3}
+            alt="Image Scroll 3"
+            className="absolute top-[1058px] left-[59px]"
+          />
+          <Image
+            src={ImgScroll4}
+            alt="Image Scroll 4"
+            className="absolute bottom-0 left-[361px]"
+          />
+        </div>
+      </section>
+
+      <Image
+        src={HeroSectionBg}
+        alt="Hero Section Background"
+        className="absolute top-0 left-0 z-0 "
+      />
+    </main>
+  );
+}
